a5a529626d658010efd4a439b3eed8eb18f30adb
pancakes2000 <47218521+jennadunford@users.noreply.github.com>
2022-04-30 13:11:43 +0200

    Added scene stuff and player movement
    Added movement
    Set up scene
    Used getkey and moveTowards for player movement

---
+++ Assets/Scripts/moveScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveScript : MonoBehaviour
+{
+
+    public Transform targetLeft;
+    public Transform targetRight;
+    public float speed = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.position = Vector3.MoveTowards(transform.position, targetLeft.position, speed);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.position = Vector3.MoveTowards(transform.position, targetRight.position, speed);
+        }
+    }
+}
