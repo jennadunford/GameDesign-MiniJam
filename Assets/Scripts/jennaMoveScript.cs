79336b7b3f7fe93f2f22608784dd745e90a87142
pancakes2000 <47218521+jennadunford@users.noreply.github.com>
2022-05-09 15:50:45 +0200

    Jenna Changed Barrier Positions & Added UI

---
+++ Assets/Scripts/jennaMoveScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jennaMoveScript : MonoBehaviour
+{
+    public Transform targetLeft;
+    public Transform targetRight;
+    public float speed = 0.05f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.position = Vector3.MoveTowards(transform.position, targetLeft.position, speed);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.position = Vector3.MoveTowards(transform.position, targetRight.position, speed);
+        }
+    }
+}
